8d5a2fedca43835757b72e1314b2e4c1150f7594
brandon <devstack@openlinux.club>
2018-03-31 01:57:01 +0700

    car_move done

---
+++ car_move/script.js
@@ -0,0 +1,54 @@
+var marginCarLeft = 0;
+var marginCarRight = 35;
+var screenSideWidth = 280;
+
+function endOfRightWall(currentPos, endWallPos)
+{
+	if( (currentPos + marginCarLeft) > endWallPos )
+	{
+		return -(marginCarLeft) - screenSideWidth;
+	}
+
+	return currentPos;
+}
+
+function endOfLeftWall(currentPos, endWallPos)
+{
+	if( (currentPos - marginCarRight) < -(screenSideWidth) )
+	{
+		return endWallPos;
+	}
+
+	return currentPos;
+}
+
+$(function () {
+
+    $(document).on('keydown', function (event) {
+
+    	var currentBackPos = parseInt($('#car').css('left'));
+    	var nextBackPos;
+
+    	// Left arrow 
+        if (event.keyCode == 37) 
+        {
+        	
+        	nextBackPos = endOfLeftWall((currentBackPos - 2), 800);
+        	$('.wheel1-line').css('animation', 'animateCircleBackward 5s linear infinite');
+            $('#car').css('left', nextBackPos);
+        }
+
+        // Right arrow 
+        if (event.keyCode == 39) 
+        {
+        	nextBackPos = endOfRightWall((currentBackPos + 2), 800);
+        	$('.wheel1-line').css('animation', 'animateCircleForward 5s linear infinite');
+            $('#car').css('left', nextBackPos);
+        }
+        
+    });
+
+    $(document).on('keyup', function (event) {
+    	$('.wheel1-line').css('animation', '');
+    });
+});
